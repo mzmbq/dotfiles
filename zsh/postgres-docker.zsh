9c336c0adcecab6a69107bd8e2e831ff9524c140
mzmbq <mzmbq@proton.me>
2024-03-09 19:04:42 +0100

    Use consistent indentation

--- zsh/postgres-docker.zsh
+++ zsh/postgres-docker.zsh
@@ -16,47 +16,47 @@ function _create_net {
 }
 
 function pgstart {
-	cd ~/docker
-	
-	_create_net
+    cd ~/docker
+    
+    _create_net
 
-	docker run --rm \
-        	-e POSTGRES_PASSWORD=secret \
-        	-e POSTGRES_USER=user \
-        	-e POSTGRES_DB=table_dev \
-    		--name=pg \
-		    -p 5432:5432 \
-    		-v pgdb:/var/lib/postgresql/data \
-		    --network pg-net \
+    docker run --rm \
+            -e POSTGRES_PASSWORD=secret \
+            -e POSTGRES_USER=user \
+            -e POSTGRES_DB=table_dev \
+            --name=pg \
+            -p 5432:5432 \
+            -v pgdb:/var/lib/postgresql/data \
+            --network pg-net \
             -d \
-    		postgres
+            postgres
 
-	cd -
+    cd -
 }
 
 function pgcon {
-	docker exec -it pg psql -U user table_dev
+    docker exec -it pg psql -U user table_dev
 }
 
 function pgastart {
-	cd ~/docker
+    cd ~/docker
 
-	_create_net
+    _create_net
 
-	docker run --rm \
-		-p 15432:80 \
-		-e 'PGADMIN_DEFAULT_EMAIL=user@user.user' \
-		-e 'PGADMIN_DEFAULT_PASSWORD=secret' \
-		--name pgadmin \
-		-v pgadmin:/var/lib/pgadmin \
-		--network pg-net \
+    docker run --rm \
+        -p 15432:80 \
+        -e 'PGADMIN_DEFAULT_EMAIL=user@user.user' \
+        -e 'PGADMIN_DEFAULT_PASSWORD=secret' \
+        --name pgadmin \
+        -v pgadmin:/var/lib/pgadmin \
+        --network pg-net \
         -d \
-		dpage/pgadmin4
+        dpage/pgadmin4
 
-	cd -
+    cd -
 }
 
 function pgstop {
-	docker stop pg
-	docker stop pgadmin 
+    docker stop pg
+    docker stop pgadmin 
 }
